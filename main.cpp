e1bdb35838ea72b35d4f9e2c6f4a40f83b6c41bf
Kylian <hsjgc1234@gmail.com>
2021-05-16 12:43:44 +0900

    answer

---
+++ main.cpp
@@ -0,0 +1,51 @@
+//
+// Created by Kylian Lee on 2021/05/16.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<long long> ordinaryNum(81);
+
+void calcOrdinaryNum();
+
+int solution(long long num);
+
+int main(){
+  int caseNum;
+  calcOrdinaryNum();
+  cin >> caseNum;
+  for (int i = 0; i < caseNum; ++i) {
+    int num;
+    cin >> num;
+    cout << solution(num) << endl;
+  }
+}
+
+void calcOrdinaryNum() {
+  int k = 0;
+  long long num;
+  for (int i = 1; i <= 9; ++i) {
+    num = i;
+    for (int j = 0; j < 9; j++) {
+      ordinaryNum[k++] = num;
+      num = num * 10 + i;
+    }
+  }
+  sort(ordinaryNum.begin(), ordinaryNum.end());
+}
+
+int solution(long long num) {
+  int answer = 0;
+  int size = ordinaryNum.size();
+  for (int i = 0; i < size; ++i) {
+    if(num >= ordinaryNum[i])
+      ++answer;
+    else
+      return answer;
+  }
+  return 81;
+}
